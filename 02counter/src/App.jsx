782340c4cbaee8edd37a0c929d4717cf8980775e
Deepanjali Kumari <deepanjalik05@gmail.com>
2025-07-31 23:32:44 -0300

    pushed counter app with basic hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react' //HOOKS
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //hooks
+  let [counter, setCounter] = useState(15)
+  //let counter = 15;
+  const addValue = () =>{
+    // console.log("value added", counter);
+      //counter = counter+1;
+      setCounter(counter +1);
+  }
+  
+  const removeValue = () =>{
+    //counter =  counter - 1;
+    setCounter(counter -1);
+  }
+
+  return (
+    <>
+      <h1>chai aur code</h1>
+      <h2>counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}>add value {counter}</button>
+      <br/>
+      <button onClick={removeValue}>remove button {counter}</button>
+    </>
+  )
+}
+
+export default App
