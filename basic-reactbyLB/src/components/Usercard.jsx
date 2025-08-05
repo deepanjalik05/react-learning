fb98711b4e1d6a4e049420983fff1dd5f3797498
Deepanjali Kumari <deepanjalik05@gmail.com>
2025-08-05 01:37:04 -0300

    made own react components

---
+++ basic-reactbyLB/src/components/Usercard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Usercard.css"
+
+const Usercard =(props) =>{
+    return (
+        <div className='user-container' style ={props.style}>
+            <p id ='user-name'>{props.name}</p>
+            <img id ='user-img' src={props.image} alt={props.name}></img>
+            <p id='user-desc'>{props.desc}</p>
+        </div>
+    )
+}
+
+export default Usercard
