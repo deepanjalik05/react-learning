57f6f4e010c231c464dc38b1e4c058871441274b
Deepanjali Kumari <deepanjalik05@gmail.com>
2025-08-05 23:07:01 -0300

    added another components of child

--- basic-reactbyLB/src/App.jsx
+++ basic-reactbyLB/src/App.jsx
@@ -1,4 +1,5 @@
 import Usercard from "./components/Usercard"
+import basicprops from "./components/basicprops"
 import './App.css'
 import me from './assets/me.jpg'
 import Gugu from './assets/Gugu.jpg'
@@ -10,7 +11,19 @@ function App() {
       <div className="container">
         <Usercard name="Deepanjali" desc ="it's me!" image ={me} style={{"border-radius": "10px"}}/>
         <Usercard name="Cat" desc ="it's Beautiful Cat" image ={cat_image} style={{"border-radius": "10px"}}/>
-        <Usercard name="Google" desc ="it's my pet" image ={Gugu} style={{"border-radius": "10px"}}/>
+        <Usercard name="Google" desc ="it's my pet" image ={Gugu} style={{"border-radius": "10px"}} />
+      </div>
+
+
+      <div>
+        <card name ="deepu">
+            <h1>Hello everyone</h1>
+            <p> this side deepanjali</p>
+        </card>
+
+        <card children ="hello how are you">
+          my pet name is google
+        </card>
       </div>
     </>
   )
