4c2a1bee3ed6ef4041caefb7e8ee810bff65643a
Deepanjali Kumari <deepanjalik05@gmail.com>
2026-01-11 20:03:04 -0300

    Ignore node_modules and dist folders

---
+++ 10todocontext/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-pink-500 text-3xl font- bold'>TO DO LIST</div>
+    </>
+  )
+}
+
+export default App
